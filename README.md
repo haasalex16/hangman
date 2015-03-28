619bf80b60892adc186dd92b3c0eb9bedf6f96dd
Alex Haas <alex.carl.haas@gmail.com>
2015-03-28 12:43:20 -0400

    Set up Hangman Game

---
+++ README.md
@@ -0,0 +1,4 @@
+HANGMAN
+by Alex Haas
+
+This is a solo project to build hangman and track my changes using Git
---
+++ hangman.rb
@@ -0,0 +1,73 @@
+class Hangman
+
+  def initialize(guessing_player, checking_player)
+    if guessing_player == 'y'
+      @guessing_player = HumanPlayer.new
+    else
+      @guessing_player = ComputerPlayer.new
+    end
+
+    if checking_player == 'y'
+      @checking_player = HumanPlayer.new
+    else
+      @checking_player = ComputerPlayer.new
+    end
+
+  end
+
+  def play
+    puts "Lets Play!"
+  end
+
+
+
+
+
+
+end
+
+
+class HumanPlayer
+
+  def pick_secret_word
+
+  end
+
+  def recieve_secret_length
+
+  end
+
+  def guess
+
+  end
+
+  def check_guess
+
+  end
+
+  def handle_guess_response
+
+
+  end
+
+
+end
+
+
+
+class ComputerPlayer
+
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Is the guessing player a Human? (y/n)"
+  guessing = gets.chomp.downcase
+  puts "Is the checking player a Human? (y/n)"
+  checking = gets.chomp.downcase
+
+  game = Hangman.new(guessing,checking)
+  game.play
+
+end
