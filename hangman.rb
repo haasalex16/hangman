2d0ff20895a105f63e4fd16cc641d231c9a02aec
Alex Haas <alex.carl.haas@gmail.com>
2015-03-28 13:53:31 -0400

    Human guesser vs comp checker complete

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,5 @@
 class Hangman
+  STRIKES = 10
 
   def initialize(guessing_player, checking_player)
     if guessing_player == 'y'
@@ -17,18 +18,41 @@ class Hangman
 
   def play
     puts "Lets Play!"
+    strikes = STRIKES
+    secret_word = @checking_player.pick_secret_word.split("")
+    display
+    puts "Strikes Remaining: #{strikes}\n\n"
+    while @checking_player.revealed_word.include?(nil) && strikes > 0
+      letter = @guessing_player.guess
+      if @checking_player.check_guess(letter)
+        @checking_player.handle_guess_response(letter)
+      else
+        strikes -= 1
+      end
+      display
+      puts "Strikes Remaining: #{strikes}\n\n"
+    end
 
-    @checking_player.pick_secret_word
-    @guessing_player.guess
-    @checking_player.check_guess
-    @checking_player.handle_guess_response
+    if @checking_player.revealed_word.include?(nil)
+      puts "Sorry You Lost"
+    else
+      "You Win!"
+    end
 
   end
 
+  def display
+    displayed_word = ""
+    @checking_player.revealed_word.each do |letter|
+      if letter == nil
+        displayed_word << "_"
+      else
+        displayed_word << letter
+      end
+    end
 
-
-
-
+    puts "Current word is: #{displayed_word}"
+  end
 
 end
 
@@ -44,7 +68,8 @@ class HumanPlayer
   end
 
   def guess
-
+    puts "Please choose a letter"
+    letter = gets.chomp.downcase
   end
 
   def check_guess
@@ -62,14 +87,16 @@ end
 
 
 class ComputerPlayer
+  attr_reader :revealed_word
 
   def initialize
-
-
+    @possible_words = File.readlines('dictionary.txt').map(&:chomp)
   end
 
   def pick_secret_word
-
+    @secret_word = @possible_words.sample
+    @revealed_word = Array.new(@secret_word.length) {nil}
+    @secret_word
   end
 
   def recieve_secret_length
@@ -80,13 +107,21 @@ class ComputerPlayer
 
   end
 
-  def check_guess
-
+  def check_guess(letter)
+    if @secret_word.include?(letter)
+      puts "Yes, my word includes '#{letter}'"
+      return true
+    else
+      puts "No, my word does not include '#{letter}'"
+      return false
+    end
   end
 
-  def handle_guess_response
 
-
+  def handle_guess_response(guess)
+    @secret_word.each_char.with_index do |letter, idx|
+      @revealed_word[idx] = letter if guess == letter
+    end
   end
 
 
